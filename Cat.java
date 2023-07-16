329e17601cd5534faf454f7d50eb47aec9425ba0
Nikitabelov365 <nikitabelov365@gmail.com>
2023-07-16 20:55:41 +0300

    init commit

---
+++ Cat.java
@@ -0,0 +1,28 @@
+public class Cat {
+    private String name;
+    private int age;
+    private String owner;
+    public Cat (String owner) {
+    this.owner = owner;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void greet() {
+        String formattedString = String.format("Мяу! Меня зовут %s. Мне %d года(лет). Мой владелец %s", name, age, owner);
+        System.out.println(formattedString);
+    }
+}
